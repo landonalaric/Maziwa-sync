869caebb3851d7a163e23538799250a5f5517aaf
landonalaric <hemsworthdavid355@gmail.com>
2026-07-23 13:22:57 +0300

    db

--- maziwasync/core/models.py
+++ maziwasync/core/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
         ('admin', 'Admin'),
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='farmer')
-    phone_number = models.CharField(max_length=15, unique=True)
+    phone_number = models.CharField(max_length=15, unique=True, blank=True, null=True)
 
     def __str__(self):
         return f"{self.username} {self.role}"
